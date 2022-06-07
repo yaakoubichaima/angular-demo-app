a1296934450a9b2ace72cf2a7c5dae46d5b4ce45
yaakoubichaima <chaimayaakoubi50@gmail.com>
2022-06-07 15:00:44 +0100

    Define and use the cart service

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -8,9 +10,11 @@ import { Product, products } from '../products';
   styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
-
   product: Product | undefined;
-  constructor(private route: ActivatedRoute) {}
+
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService) {}
 
   ngOnInit() {
     //getting the id from the current route
@@ -18,7 +22,14 @@ export class ProductDetailsComponent implements OnInit {
     const productIdFromRoute = Number(routeParams.get('productId'));
 
     //Finding the corresponded product
-    this.product = products.find(product => product.id === productIdFromRoute);
-
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
   }
+
+  addToCart(product: Product){
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!')
+  }
+
 }
